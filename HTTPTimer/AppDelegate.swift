6f454993e3195af8167521a3c7fb6e38df15c65e
Christian Schneider <christian@Christians-MacBook-Pro.fritz.box>
2015-08-15 22:52:30 +0200

    Initial Commit

---
+++ HTTPTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HTTPTimer
+//
+//  Created by Christian Schneider on 15.08.15.
+//  Copyright (c) 2015 NonameCompany. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HTTPTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HTTPTimer
+//
+//  Created by Christian Schneider on 15.08.15.
+//  Copyright (c) 2015 NonameCompany. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
