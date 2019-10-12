87a6404e7913fdd82696f5388658d313c81fbe5d
Christian Schneider <christian.schneider3@gmx.net>
2019-10-12 10:54:37 +0200

    closes #4 added app icon
    Signed-off-by: Christian Schneider <christian.schneider3@gmx.net>

---
+++ HTTPTimer/graphics/appicon/generateLogos.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+resolutions=(
+	16
+	32
+	64
+	128
+	256
+	512
+	1024
+)
+
+inputimage=$1
+
+if [[ ${inputimage} != *.png ]]
+then
+	echo Input images must be of type png due to transparancy.
+	exit
+fi
+
+for i in "${resolutions[@]}"
+do
+	sips --resampleWidth "$i" $inputimage --out "$i"x"$i".png
+	echo Image "$i" generated
+done
--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 [![GitHub issues](https://img.shields.io/github/issues/chrisschnei/HTTPTimer)](https://github.com/chrisschnei/HTTPTimer/issues)
 
 This macOS app will periodically send HTTP requests to a given domain.
+
+![macOS App](HTTPTimer/graphics/appicon/128x128.png)
+
+![macOS App](HTTPTimer/graphics/appUI/appUI.jpg)
